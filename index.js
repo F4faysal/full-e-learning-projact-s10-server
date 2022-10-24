3da4e600a320383581e84dfe617471702b132b06
f4faysal <f4faysals@gmai.com.com>
2022-10-24 22:07:35 +0600

    creat a face data

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
-
 app.use(cors())
 
-
+const courses = require('./data/coursesProgram.json')
 
 app.get('/', (req, res) => {
   res.send('data loding....')
 })
+app.get('/courses', (req, res) => {
+    res.send(courses)
+  })
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
